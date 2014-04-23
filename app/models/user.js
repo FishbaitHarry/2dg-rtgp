c003433b32120a08ed55e96deba986e0e32cf489
Szymon Maj <fishbaitharry@gmail.com>
2014-04-23 22:41:09 +0200

    deliveries added

--- app/models/user.js
+++ app/models/user.js
@@ -1,28 +1,18 @@
 var User = function () {
 
   this.defineProperties({
-    name: {type: 'string', required: true},
-    email: {type: 'string', required: true}
+    name:      {type: 'string', required: true},
+    email:     {type: 'string', required: true},
+    password:  {type: 'string'},
+    role:      {type: 'string'},
+    lastLogin: {type: 'time'}
   });
-
+ 
   this.validatesLength('name', {min: 3});
+  this.validatesFormat('role', /admin|master|user/, {message: 'Illegal role!'});
 
   this.hasMany('PrivateMessages');
   /*
-  this.property('login', 'string', {required: true});
-  this.property('password', 'string', {required: true});
-  this.property('lastName', 'string');
-  this.property('firstName', 'string');
-
-  this.validatesPresent('login');
-  this.validatesFormat('login', /[a-z]+/, {message: 'Subdivisions!'});
-  this.validatesLength('login', {min: 3});
-  // Use with the name of the other parameter to compare with
-  this.validatesConfirmed('password', 'confirmPassword');
-  // Use with any function that returns a Boolean
-  this.validatesWithFunction('password', function (s) {
-      return s.length > 0;
-  });
 
   // Can define methods for instances like this
   this.someMethod = function () {
@@ -32,15 +22,20 @@ var User = function () {
 
 };
 
+User.login = function login(email, password, callback) {
+  User.first({email:email}, userFound);
+  function userFound(err, user) {
+    // validate password here
+    callback(err, user);
+  }
+};
+
 /*
 // Can also define them on the prototype
 User.prototype.someOtherMethod = function () {
   // Do some other stuff
 };
 // Can also define static methods and properties
-User.someStaticMethod = function () {
-  // Do some other stuff
-};
 User.someStaticProperty = 'YYZ';
 */
 
