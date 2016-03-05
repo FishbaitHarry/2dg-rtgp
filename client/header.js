cd993357f2df6b12d7215cc6b315464ef6d0df7c
Szymon Maj <szymon.maj@ocado.com>
2016-03-05 19:01:40 +0100

    added bootstrap

--- client/header.js
+++ client/header.js
@@ -1,5 +1,5 @@
 Template.header.events({
-    'click nav a': function (event) {
+    'click .clickable a': function (event) {
         var href = event.target.href;
         var targetPage = href.slice(href.indexOf('#') + 1);
         Session.set('page', targetPage);
